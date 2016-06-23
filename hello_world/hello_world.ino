40689a464161eafaaa5c069e567a6c475618ff17
zooroo <zooroo@foxmail.com>
2016-06-23 10:06:16 +0800

    Blink
    the Blink sketch

--- hello_world/hello_world.ino
+++ hello_world/hello_world.ino
@@ -1,8 +1,13 @@
+#define LEDPIN 13
+
 void setup(){
-  
+  pinMode(LEDPIN,OUTPUT);
 }
 
 void loop(){
-  
+  digitalWrite(LEDPIN,HIGH);
+  delay(1000);
+  digitalWrite(LEDPIN,LOW);
+  delay(1000);
 }
 
